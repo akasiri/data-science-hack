8509f364e2c68c770fb395af4e385a5a5af8dac4
akasiri <akasiri@uci.edu>
2016-05-14 21:35:55 -0700

    Add files via upload
    aaaaa

--- parse_raw_data.py
+++ parse_raw_data.py
@@ -1,14 +1,11 @@
 import json
-app_usage_events_column = 'entry_date device_id application_version_id type start_date run_time end_date continuation'.split()
-application_versions_column = 'application_version_id package_name name version develop'ype catergor
 
 def txt_parse(file_path):
     ls = []
-    with open(file_path, "r") as f:
+    with open(file_path, "rb") as f:
         for line in f:
-            print(line)
             l = []
-            for token in str(line).split(','):
+            for token in str(line).strip().split(','):
                 l.append(token)
             ls.append(l)
     return ls
@@ -23,9 +20,9 @@ if __name__ == "__main__":
     with open("device_dict.json", "w") as f:
         device_dict = build_dict(txt_parse("../M2C Final Data/devices.csv"))
         f.write(json.dumps(device_dict))
-    with open("log_list.json", "w") as f:
-        log_list = txt_parse("../M2C Final Data/log_data/.csv")
-        f.write(json.dumps(device_dict))
-    with open("app_list.json", "w") as f:
-        log_list = txt_parse("../M2C Final Data/application_versions.csv")
-        f.write(json.dumps(device_dict))
+    with open("log_9-1_list.txtdata", "w") as f:
+        log_list = txt_parse("../M2C Final Data/log_data/log_data_2015_09_01.csv")
+        f.write(json.dumps(log_list))
+    with open("app_list.txtdata", "w") as f:
+        app_list = txt_parse("../M2C Final Data/application_versions.csv")
+        f.write(json.dumps(app_list))
